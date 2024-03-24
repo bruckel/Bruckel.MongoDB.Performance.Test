75d17e8b62c819e5e4c40898cc4f69139b01c223
Tomás Brückel Ruiz <tomasruizbruckel@gmail.com>
2024-03-24 21:52:06 +0100

    Commit inicial del proyecto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("Hola Mundo!");
+
+internal class Coefficient
+{
+    public required DateTime TimeStamp { get; set; } 
+    
+    public required string Cups { get; set; } 
+
+    public required double Value { get; set; } 
+}
