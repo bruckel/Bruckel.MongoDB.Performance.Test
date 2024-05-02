6346a5a276fcc1cd544e3c1d95badbc8d062802e
Tomás Brückel Ruiz <tomasruizbruckel@gmail.com>
2024-05-02 23:41:29 +0200

    Crear colección TimeSeries e insertar 100 * un año entero.

--- Program.cs
+++ Program.cs
@@ -17,14 +17,14 @@ do
 }
 while (cups.Count < 100);
 
-var coefficients = cups.SelectMany(c => hours.Select(h => new Coefficient
+var coefficients = cups.SelectMany(c => hours.Select(h => new CoefficientTimeStamp
 {
-    TimeStamp = h,
-    Cups = c,
+    TimeStamp = h.ToUniversalTime(),
+    Metadata = new CoefficientMetadata { Cups = c},
     Value = random.NextDouble()
 }));
 
-var batches = new List<List<Coefficient>>();
+var batches = new List<List<CoefficientTimeStamp>>();
 var batchSize = 1000;
 var count = 0;
 
@@ -42,15 +42,37 @@ while (batches.SelectMany(b => b.Select(s => s)).Count() < coefficients.Count())
 var watch = new Stopwatch();
 watch.Start();
 
-const string connectionUri = "mongodb+srv://tomas:Cantabria30011978@cluster0.rowja.azure.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+//const string connectionUri = "mongodb+srv://tomas:Cantabria30011978@cluster0.rowja.azure.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-var settings = MongoClientSettings.FromConnectionString(connectionUri);
+var settings = MongoClientSettings.FromConnectionString("mongodb://root:root@localhost:27777/?directConnection=true");
 
 // Set the ServerApi field of the settings object to set the version of the Stable API on the client
 settings.ServerApi = new ServerApi(ServerApiVersion.V1);
 
-// Create a new client and connect to the server
+var seconds = 0.0;
+
 var client = new MongoClient(settings);
+var database = client.GetDatabase("temp");
+var hasCollection = database.ListCollectionNames().ToList().Any(l => l == "coefficients");
+if (!hasCollection)
+{
+    database.CreateCollection("coefficients", new CreateCollectionOptions { TimeSeriesOptions = new TimeSeriesOptions(timeField: "TimeStamp", metaField: "Metadata", granularity: TimeSeriesGranularity.Hours) });
+    var newCollection = database.GetCollection<CoefficientTimeStamp>("coefficients");
+    
+    foreach (var batch in batches)
+    {
+        await newCollection.InsertManyAsync(batch);
+        seconds += watch.ElapsedMilliseconds/1000;
+        Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds/1000}");
+    }
+}
+
+//var collection = database.GetCollection<CoefficientTimeStamp>("coefficients");
+//await collection.InsertManyAsync(batches.SelectMany(b => b));
+//Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds/1000}");
+
+// Create a new client and connect to the server
+/*var client = new MongoClient(settings);
 var collection = client.GetDatabase("Tempelhof").GetCollection<Coefficient>("FacilityCoefficients");
 if (collection is not null)
 {
@@ -59,15 +81,20 @@ if (collection is not null)
         await collection.InsertManyAsync(batch);
         Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds}");
     }
-}
+}*/
 
-Console.WriteLine("Finished");
+Console.WriteLine($"Finish time: {seconds}");
 
-internal class Coefficient
+internal class CoefficientTimeStamp
 {
-    public required DateTime TimeStamp { get; set; }
+    public required BsonDateTime TimeStamp { get; set; }
 
-    public required string Cups { get; set; }
+    public required CoefficientMetadata Metadata { get; set; }
 
     public required double Value { get; set; }
+}
+
+internal class CoefficientMetadata
+{
+    public required string Cups { get; set; }
 }
